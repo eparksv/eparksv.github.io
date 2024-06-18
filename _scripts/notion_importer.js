9e62a0ca059471c690e258dcca2fea01982f0910
E. Park <epark.grace@gmail.com>
2024-06-18 23:52:55 +0900

    노션 페이지 설정

---
+++ _scripts/notion_importer.js
@@ -0,0 +1,178 @@
+const { Client } = require("@notionhq/client");
+const { NotionToMarkdown } = require("notion-to-md");
+const moment = require("moment");
+const path = require("path");
+const fs = require("fs");
+const axios = require("axios");
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+function escapeCodeBlock(body) {
+  const regex = /```([\s\S]*?)```/g;
+  return body.replace(regex, function (match, htmlBlock) {
+    return "\n{% raw %}\n```" + htmlBlock.trim() + "\n```\n{% endraw %}\n";
+  });
+}
+
+function replaceTitleOutsideRawBlocks(body) {
+  const rawBlocks = [];
+  const placeholder = "%%RAW_BLOCK%%";
+  body = body.replace(/{% raw %}[\s\S]*?{% endraw %}/g, (match) => {
+    rawBlocks.push(match);
+    return placeholder;
+  });
+
+  const regex = /\n#[^\n]+\n/g;
+  body = body.replace(regex, function (match) {
+    return "\n" + match.replace("\n#", "\n##");
+  });
+
+  rawBlocks.forEach(block => {
+    body = body.replace(placeholder, block);
+  });
+
+  return body;
+}
+
+// passing notion client to the option
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+(async () => {
+  // ensure directory exists
+  const root = "_posts";
+  fs.mkdirSync(root, { recursive: true });
+
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  let response = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      property: "Publish",
+      checkbox: {
+        equals: true,
+      },
+    },
+  });
+
+  const pages = response.results;
+  while (response.has_more) {
+    const nextCursor = response.next_cursor;
+    response = await notion.databases.query({
+      database_id: databaseId,
+      start_cursor: nextCursor,
+      filter: {
+        property: "Publish",
+        checkbox: {
+          equals: true,
+        },
+      },
+    });
+    pages.push(...response.results);
+  }
+
+  for (const r of pages) {
+    const id = r.id;
+    // date
+    let date = moment(r.created_time).format("YYYY-MM-DD");
+    let pdate = r.properties?.["Published time"]?.["date"]?.["start"];
+    if (pdate) {
+      date = moment(pdate).format("YYYY-MM-DD");
+    }
+    // title
+    let title = id;
+    let ptitle = r.properties?.["Name"]?.["title"];
+    if (ptitle?.length > 0) {
+      title = ptitle[0]?.["plain_text"];
+    }
+    // tags
+    let tags = [];
+    let ptags = r.properties?.["Topics"]?.["relation"];
+    for (const t of ptags) {
+      let tagNames = await notion.pages.properties.retrieve({
+        page_id: t.id,
+        property_id: "Name"
+      });
+      const n = tagNames?.["results"]?.[0]?.["title"]?.["plain_text"];
+      if (n) {
+        tags.push(n);
+      }
+    }
+    // categories
+    let cats = [];
+    let pcats = r.properties?.["Notebooks"]?.["relation"];
+    for (const t of pcats) {
+      let catNames = await notion.pages.properties.retrieve({
+        page_id: t.id,
+        property_id: "Name"
+      });
+      const n = catNames?.["results"]?.[0]?.["title"]?.["plain_text"];
+      if (n) {
+        cats.push(n);
+      }
+    }
+
+    // frontmatter
+    let fmtags = "";
+    let fmcats = "";
+    if (tags.length > 0) {
+      fmtags += "\ntags: [" + tags.join(", ") + "]";
+    }
+    if (cats.length > 0) {
+      fmcats += "\ncategories: [" + cats.join(", ") + "]";
+    }
+    const fm = `---
+layout: post
+date: ${date}
+title: "${title}"${fmtags}${fmcats}
+---
+`;
+    const mdblocks = await n2m.pageToMarkdown(id);
+    let md = n2m.toMarkdownString(mdblocks)["parent"];
+    if (md === undefined || md === "") {
+      continue;
+    }
+    md = escapeCodeBlock(md);
+    md = replaceTitleOutsideRawBlocks(md);
+
+    const ftitle = `${date}-${id}.md`;
+
+    let index = 0;
+    let edited_md = md.replace(
+      /!\[(.*?)\]\((.*?)\)/g,
+      function (match, p1, p2, p3) {
+        const dirname = path.join("assets/img", ftitle);
+        if (!fs.existsSync(dirname)) {
+          fs.mkdirSync(dirname, { recursive: true });
+        }
+        const filename = path.join(dirname, `${index}.png`);
+
+        axios({
+          method: "get",
+          url: p2,
+          responseType: "stream",
+        })
+        .then(function (response) {
+          let file = fs.createWriteStream(`${filename}`);
+          response.data.pipe(file);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+        let res;
+        if (p1 === "") res = "";
+        else res = `_${p1}_`;
+
+        return `![${index++}](/${filename})${res}`;
+      }
+    );
+
+    //writing to file
+    fs.writeFile(path.join(root, ftitle), fm + edited_md, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+})();
